b78349ddff99d354eabe80bac03007a838f80435
YOUSSEF Amel <G521763@mph.morpho.com>
2020-09-17 09:14:29 +0200

    adding button load more and   the filters is one dropdown and textbox

--- src/app/views/news-listing-releases/news-listing-releases.component.ts
+++ src/app/views/news-listing-releases/news-listing-releases.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { SharingDataService } from '../../_services';
+import * as newaapi from './../../_files/newsapi.json';
 
 @Component({
   selector: 'app-news-listing-releases',
@@ -7,15 +9,52 @@ import { SharingDataService } from '../../_services';
   styleUrls: ['./news-listing-releases.component.scss']
 })
 export class NewsListingReleasesComponent implements OnInit {
-  articles: any;
-
-  constructor(private sharingDataService: SharingDataService) { }
+  articles: any[];
+  categories: { id: number; name: string; }[];
+  myGroup = new FormGroup({
+    title: new FormControl(''),
+    category: new FormControl('')
+ });
+  articlesClone: any[];
+  isExistedMore: boolean;
+  constructor() { }
 
   ngOnInit() {
-    this.articles = this.sharingDataService.articles;
-    if (!this.articles) {
-      this.articles =  JSON.parse(sessionStorage.getItem('articles'));
+    this.categories = newaapi.sourceCategory;
+    this.articles = newaapi.articles.
+    sort((item: any, item2: any) =>  {
+      const c: any = new Date(item.publishedAt);
+      const d: any = new Date(item2.publishedAt);
+      return d - c;
+    });
+    this.articlesClone = this.articles.slice();
+    this.articles = this.articles.slice(0, 8);
+    this.isExistedMoreItems();
+  }
+  getFilterData() {
+    console.log('getFilterData',  this.myGroup);
+    if (this.myGroup.value.category != '' && this.myGroup.value.title != '') {
+    this.articles = this.articlesClone.filter((item) => item.sourceID == this.myGroup.value.category
+    && item.title.includes(this.myGroup.value.title));
+    return this.articles;
+    } else if (this.myGroup.value.title != '') {
+      this.articles = this.articlesClone.filter((item) => item.title.includes(this.myGroup.value.title));
+    return this.articles;
+    } else if (this.myGroup.value.category != '' ) {
+      this.articles = this.articlesClone.filter((item) => item.sourceID == this.myGroup.value.category);
+    return this.articles;
+    } else {
+     return this.articles = this.articlesClone.slice(0, 8);
     }
   }
-
+  getNextData() {
+    this.articles = this.articlesClone.slice(0, this.articles.length + 4);
+    this.isExistedMoreItems();
+  }
+  isExistedMoreItems() {
+    this.isExistedMore = true;
+     if (this.articlesClone.length == this.articles.length) {
+       this.isExistedMore = false;
+     }
+  }
 }
